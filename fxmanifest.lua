12435f2a61e43d3459788815672bd8fe57b48eca
BlackzoneV <74337524+BlackzoneV@users.noreply.github.com>
2020-11-12 06:32:43 +0300

    bz_arackirala

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX'
+
+client_scripts {
+	'@extendedmode/locale.lua',
+	'config.lua',
+	'c_arackirala.lua'
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@extendedmode/locale.lua',
+	'config.lua',
+	's_arackirala.lua'
+}
+
+
